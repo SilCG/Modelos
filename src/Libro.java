637138ce5cf19bb19dfe444974c8e3d6ff20af1a
Silvia Calvo Gonzalez <scalvog@ucenfotec.ac.cr>
2026-09-24 19:32:35 -0600

    Segundo respaldo con la creación de Objetos

--- src/Libro.java
+++ src/Libro.java
@@ -5,4 +5,27 @@ public class Libro {
     String autor;
     String genero;
     short anioPublicacion;
+
+    // Métodos
+    // Constructor completo
+    Libro(String titulo, String autor, String genero, short anioPublicacion) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    // Constructor parcial (sobrecargado)
+    Libro(String titulo,String autor){
+        this.titulo = titulo;
+        this.autor = autor;
+        genero = null;
+        anioPublicacion = 1800;
+    }
+
+    // Contructor por defecto (dejando parantesis abierto
+    Libro (){}
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
+
+    // public static InputStreamReader lector = new InputStreamReader(System.in); System.in es como decirle: Lea del teclado
+    // public static BufferedReader entrada = new BufferedReader(lector); Uso el lector como intermediario
+    // Pero también puedo pasar la construcción del objeto al BufferedReader y borro toda esa primera línea, quedaría así:
+
+    public static BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in)); // Uso el lector como intermediario
 
     public static void main (String[] args) {
 
         // Modelo de un libro
         // String titulo = "Le Rouge et le Noir";
+        // String autor = "Stendhal";
         // String autor = "Stendhal";
         // String genero = "Novela";
         // short anioPublicacion = 1830; // Short representa un número entero que ocupa 2 bytes en memoria. [-32768, 32767]
@@ -12,13 +22,14 @@ public class Main {
         // sout + TAB
 
         // 1. Construcción del objeto
-        Libro libro1 = new Libro();
+        Libro libro1 = new Libro("Cien años de soledad","Gabriel García Márquez", "Novela", (short)1967);
 
         // 2. Asignación de los atributos del objeto
-        libro1.titulo = "Cien años de soledad";
-        libro1.autor = "Gabriel García Márquez";
-        libro1.genero = "Novela";
-        libro1.anioPublicacion = 1967;
+        // Una vez que se copiar arribas estas 4 lineas de abajo.
+        // libro1.titulo = "Cien años de soledad";
+        // libro1.autor = "Gabriel García Márquez";
+        // libro1.genero = "Novela";
+        // libro1.anioPublicacion = 1967;
 
         System.out.println("El libro se titula " + libro1.titulo + ", y es un(a) " + libro1.genero + " publicado(a) por " + libro1.autor + " en " + libro1.anioPublicacion);
 
